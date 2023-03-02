2671e83e57be27e4a897f89f7692b34b8760b01e
JohnDonavon <john.stephens95@gmail.com>
2023-03-01 16:38:03 -0800

    and other stuff

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -26,13 +26,14 @@ pub use program::*;
 pub(crate) mod types;
 pub(crate) use types::*;
 
-pub use wasm_bindgen_rayon::init_thread_pool;
+pub use snarkvm_algorithms::init_thread_pool;
 
 use wasm_bindgen::prelude::*;
-use rayon::iter::IntoParallelRefIterator;
+use rayon::iter::IntoParallelIterator;
+use rayon::iter::*;
 use rayon::iter::ParallelIterator;
 
 #[wasm_bindgen]
 pub fn sum(numbers: &[i32]) -> i32 {
-  numbers.par_iter().sum()
+  numbers.into_par_iter().with_max_len(1).sum()
 }
