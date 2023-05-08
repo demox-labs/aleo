e30dda1a603773f08ad7df5debd31d5948d00b29
fulltimemike <mike@demoxlabs.xyz>
2023-05-08 18:06:33 -0500

    mul and invert fields

--- wasm/src/account/address.rs
+++ wasm/src/account/address.rs
@@ -21,7 +21,7 @@ use crate::{
 
 use core::{convert::TryFrom, fmt, ops::Deref, str::FromStr};
 use aleo_rust::{Network, Field};
-use snarkvm_wasm::{FromBytes, program::{ProjectiveCurve, Environment, Double}};
+use snarkvm_wasm::{FromBytes, program::{ProjectiveCurve, Environment, Double, Inverse}, types::Group};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -85,9 +85,22 @@ impl Address {
         result.to_string()
     }
 
+    pub fn invert_field(field: &str) -> String {
+        let field = Field::<CurrentNetwork>::from_str(field).unwrap();
+        let result = field.inverse().unwrap();
+        result.to_string()
+    }
+
     pub fn double_field(field: &str) -> String {
         let field = Field::<CurrentNetwork>::from_str(field).unwrap();
         let result = field.double();
+        result.to_string()
+    }
+
+    pub fn mul_fields(field1: &str, field2: &str) -> String {
+        let field1 = Field::<CurrentNetwork>::from_str(field1).unwrap();
+        let field2 = Field::<CurrentNetwork>::from_str(field2).unwrap();
+        let result = field1 * field2;
         result.to_string()
     }
 
@@ -95,6 +108,12 @@ impl Address {
         let field = Field::<CurrentNetwork>::from_str(field).unwrap();
         let result = CurrentNetwork::hash_many_psd8(&[CurrentNetwork::encryption_domain(), field], 1);
         return result[0].to_string();
+    }
+
+    pub fn affine_to_projective(group: &str) -> String {
+        let group = Group::<CurrentNetwork>::from_str(group).unwrap();
+        return group.x.to_string();
+        // return "Projective: { x: ".to_string() + &group.x.to_string() + ", y: " + &group.y.to_string() + ", z: " + &group.z.to_string() + ", t: " + &group.t.to_string() + " }";
     }
 }
 
